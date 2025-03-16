59bf07ecc7f040bed71cec3fea7226b21bb9408f
Rémi Connesson <remiconnesson2@gmail.com>
2025-03-16 15:47:34 +0000

    Add store

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+// access the `store` variable anywhere in the component ✨
+const store = useCounterStore()
+</script>
+
 <template>
   <UApp>
     <NuxtPage />
